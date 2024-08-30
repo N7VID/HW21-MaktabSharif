c1a9de28c6147edda680cdb90eae1ad89319dc65
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-30 21:46:32 +0330

    fix: style homepage

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,39 +1,49 @@
-import { Stack, Typography } from "@mui/material";
+import { Paper, Stack, Typography } from "@mui/material";
 import MuiButton from "../../components/MuiButton/MuiButton";
 import styles from "./index.module.css";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <Stack className={styles.main} height={"100vh"} position={"relative"}>
-      <Stack
-        position={"absolute"}
-        top={260}
-        right={300}
-        alignItems={"center"}
-        gap={3}
-      >
-        <Typography variant="h2">Online Courses</Typography>
-        <Stack>
-          <Typography variant="subtitle1" textAlign={"center"}>
-            Master new skills and advance your career
+    <Stack
+      className={styles.main}
+      height={"100vh"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Stack alignItems={"center"} gap={3}>
+        <Paper
+          elevation={10}
+          sx={{
+            gap: "25px",
+            padding: "40px",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h3" fontWeight={600} my={"10px"}>
+            Online Courses
           </Typography>
-          <Typography variant="subtitle1" textAlign={"center"}>
-            with top-quality courses designed for success
-          </Typography>
-        </Stack>
-        <Stack spacing={2} direction={"row"}>
-          <Link to={"/login"}>
-            <MuiButton color={"error"} component={"a"}>
-              Login
-            </MuiButton>
-          </Link>
-          <Link to={"signUp"}>
-            <MuiButton color={"error"} component={"a"}>
-              Sign up
-            </MuiButton>
-          </Link>
-        </Stack>
+          <Stack>
+            <Typography variant="subtitle1" textAlign={"center"}>
+              Master new skills and advance your career
+            </Typography>
+            <Typography variant="subtitle1" textAlign={"center"}>
+              with top-quality courses designed for success
+            </Typography>
+          </Stack>
+          <Stack spacing={2} direction={"row"}>
+            <Link to={"/login"}>
+              <MuiButton component={"a"}>Login</MuiButton>
+            </Link>
+            <Link to={"signUp"}>
+              <MuiButton component={"a"}>Sign up</MuiButton>
+            </Link>
+          </Stack>
+        </Paper>
       </Stack>
     </Stack>
   );
